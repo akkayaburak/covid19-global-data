5adf7a30b7716c2cf3023da5848d17cf7618e210
Burak Akkaya <bakkaya@MagiClick.com>
2020-10-05 13:46:21 +0300

    styles

--- Covid19GlobalData/wwwroot/js/request.js
+++ Covid19GlobalData/wwwroot/js/request.js
@@ -7,7 +7,7 @@
             $(".cv-data").remove();
             $.each(result.dailyCovids, function (key, value) {
                 console.log(value);
-                var string = '<tr class="cv-data"><th scope = "row" ></th><td>' + value.country + '</td><td>' + value.countryCode + '</td><td>' + value.cumulativeDeaths + '</td><td>' + value.newCases + '</td><td>' + value.cumulativeDeaths + '</td><td>' + value.whoRegion + '</td><td>' + value.newDeaths + '</td><td>' + value.dateReported + '</td></tr >'
+                var string = '<tr class="cv-data"></th><td>' + value.country + '</td><td>' + value.cumulativeDeaths + '</td><td>' + value.countryCode + '</td><td>' + value.newCases + '</td><td>' + value.cumulativeDeaths + '</td><td>' + value.whoRegion + '</td><td>' + value.newDeaths + '</td><td>' + value.dateReported + '</td></tr >'
                 $('#cvDataTbl').append(string);
             });
 
